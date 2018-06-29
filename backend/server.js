807fbe569f13865b719d923ff2c2ed736e188a72
stacief510 <staciemfraser@gmail.com>
2018-06-29 14:22:26 -0700

    updated server home route

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-	res.send(200, 'Server Working...', {root: __dirname});
-
+	res.send('Server Working...');
 });
 
 
