ff2dcec31c4783b73287f0de7adc7390ce79301c
stacief510 <staciemfraser@gmail.com>
2018-07-01 18:35:10 -0700

    updated layout of Cities page

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -28,16 +28,18 @@ class Cities extends Component{
                             <img alt="cityImg" src=""/>
                         </div>
                         </div>
-
-                        <div className="postList row">
-
-                        <button type="button" class="btn btn-primary" data-toggle="postModal" data-target="#postModal">
+                        <button className="btn btn-info create" data-toggle="modal" data-target="#postModal">
                         Create a New Post
                         </button>
-                            <h1>Posts</h1>
+                        <h1>Posts</h1>
+                        <div className="postList row">
+                           <div className="col-4">
                             <img alt="postImg" src=""/>
+                            </div>
+                            <div className="col-8">
                             <h3>Title</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                            </div>
                         </div>
                     </section>
                     </div>
