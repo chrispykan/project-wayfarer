910f30036f7ec9e6ba504e9eeea72032eb4ce279
chrispykan <christineparisk@gmail.com>
2018-07-02 10:06:03 -0700

    up to date

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,42 +1,42 @@
-// import React, { Component } from 'react'
+import React, { Component } from 'react'
 
-// class LoginForm extends Component {
+class LoginForm extends Component {
   
-//   render() {
-//     return (
+  render() {
+    return (
      
-//       <div className="modal fade" id="#popUpLogin" >
-//       <div className="modl-dialog">
-//         <div className="modal-content">
-//           {/* Login Header */}
-//           <div className="modal-header">
-//            <button type="button" className="close" data-dismiss="modal">&times;</button>
-//           </div>
-//           {/* Login Form */}
-//           <div className="modal-body">
-//             <form role="form">
-//               <div className="form-group">
-//                 <label>username</label>
-//                 <input type="emial" className="form-control"/>
-//               </div>    
-//               <div className="form-group">
-//                 <label>password</label>
-//                 <input type="password" className="form-control"/>
-//               </div>         
-//             </form>
-//           </div>
+      <div className="modal fade" id="#popUpLogin" >
+      <div className="modl-dialog">
+        <div className="modal-content">
+          {/* Login Header */}
+          <div className="modal-header">
+           <button type="button" className="close" data-dismiss="modal">&times;</button>
+          </div>
+          {/* Login Form */}
+          <div className="modal-body">
+            <form role="form">
+              <div className="form-group">
+                <label>username</label>
+                <input type="emial" className="form-control"/>
+              </div>    
+              <div className="form-group">
+                <label>password</label>
+                <input type="password" className="form-control"/>
+              </div>         
+            </form>
+          </div>
 
-//         {/* Login button inside form */}
-//         <div className="modal-footer">
-//         <button class="btn btn-primary">Log In</button>
-//         </div>
+        {/* Login button inside form */}
+        <div className="modal-footer">
+        <button class="btn btn-primary">Log In</button>
+        </div>
 
-//         </div>
-//       </div>
-//   </div>
+        </div>
+      </div>
+  </div>
 
-//     )
-//   }
-// }
+    )
+  }
+}
 
-// export default LoginForm
+export default LoginForm
