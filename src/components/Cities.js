b105a906fbd84536ae6a2520f4e5e4e9881e4c11
stacief510 <staciemfraser@gmail.com>
2018-07-01 18:50:04 -0700

    updated styles on aside of cities page

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -7,16 +7,31 @@ class Cities extends Component{
             
               <div className="container">
               <div className="row">
-                <div className="col-4">
-                        <aside className="cityList">
+                <div className="col-5">
+                        <aside>
                             <h3>Cities</h3>
-                            <a>London </a>
-                            <a>Sydney</a>
-                            <a>San Francisco</a>
-                            <a>Seattle</a>
+                            <div className="cityList">
+                                <img className="thumbnailCityImg" alt="LondonImg" src="/images/london.jpg"/>
+                                <a>London </a>
+                            </div>
+
+                            <div className="cityList">
+                                <img className="thumbnailCityImg" alt="SydneyImg" src="/images/sydney.jpg"/>
+                                <a>Sydney</a>
+                            </div>
+
+                            <div className="cityList">
+                                <img  className="thumbnailCityImg"alt="SFImg" src="/images/bay-bridge.jpg"/>
+                                <a>San Francisco</a>
+                            </div>
+
+                            <div className="cityList"> 
+                                <img className="thumbnailCityImg" alt="SeattleImg" src="/images/seattle.jpg"/>
+                                <a>Seattle</a>
+                            </div>
                         </aside>
                     </div>
-                    <div className="col-8">
+                    <div className="col-7">
                     <section>
                         <div className="row cityInfo">
                             <div className="col-3">
